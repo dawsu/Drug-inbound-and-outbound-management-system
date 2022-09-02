efee577a4f714f3c7ae56ca0ab3e5e7329918b64
SuDaw <70442992+dawsu@users.noreply.github.com>
2022-09-02 14:26:47 +0800

    Update VariableTable.java

--- DMS/src/main/java/link/VariableTable.java
+++ DMS/src/main/java/link/VariableTable.java
@@ -2,14 +2,16 @@ package link;
 public class VariableTable {
 	public static final String url = "jdbc:mysql://localhost:3306/web?serverTimezone=UTC";
 	
-	public static final String user = "root";
-	public static final String password = "123456";
+	public static final String user = "你的mysql账号";
+	public static final String password = "你的mysql密码";
 	
+	//表字段
 	public static final String TABLE_FIEST ="id";
 	public static final String TABLE_SECOND ="name";
 	public static final String TABLE_THIRD ="num";
 	public static final String TABLE_FOURTH ="date";
 	
+	//表名
 	public static final String TABLE_SUSER = "drugs";
 	public static final String TABLE_SUSER2 = "bill";
 
